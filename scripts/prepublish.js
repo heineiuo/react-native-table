acea401e3cf688fc0dae97a146df73ce81c411e2
heineiuo <heineiuo@gmail.com>
2022-02-28 20:35:44 +0800

    feat: Add github action

---
+++ scripts/prepublish.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+
+const pkgFile = path.resolve(__dirname, "../package.json");
+const pkg = JSON.parse(fs.readFileSync(pkgFile, "utf-8"));
+
+pkg.main = "lib/commonjs/index.js";
+
+fs.writeFileSync(pkgFile, JSON.stringify(pkg, null, 2), "utf-8");
