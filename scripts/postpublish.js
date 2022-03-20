3c3da5101d6b19cea60d3a0ccaa6982060f41c9b
heineiuo <heineiuo@gmail.com>
2022-03-20 10:29:57 +0800

    v0.3.0

--- scripts/postpublish.js
+++ scripts/postpublish.js
@@ -2,8 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const pkgFile = path.resolve(__dirname, "../package.json");
-const pkg = JSON.parse(fs.readFileSync(pkgFile, "utf-8"));
+const backupPkgFile = path.resolve(__dirname, "../package-original.json");
 
-pkg.main = "node_modules/expo/AppEntry.js";
-
-fs.writeFileSync(pkgFile, JSON.stringify(pkg, null, 2), "utf-8");
+fs.unlinkSync(pkgFile);
+fs.renameSync(backupPkgFile, pkgFile);
