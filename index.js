bd5e25975047f9db998f4c4aade6c9d9684f54c5
tohsaka888 <156132264@qq.com>
2022-12-18 16:04:43 +0800

    ♻️ refactor: refactor entry

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ import { Platform } from "react-native";
 
 import App from "./App";
 
-
 // @see https://github.com/expo/expo/issues/18485
 if (Platform.OS === "web") {
-  const rootTag = createRoot(
-    document.getElementById("root") ?? document.getElementById("main")
-  );
+  const rootTag = createRoot(document.getElementById("root"));
   rootTag.render(createElement(App));
 } else {
   registerRootComponent(App);
